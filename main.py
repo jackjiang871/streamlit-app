73d9ae0fc21a9f4c77112b569a99d9cd93411b8c
Jack Jiang <jackjiang871@gmail.com>
2024-01-06 11:55:02 -0800

    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="CollegeGPT",
+    page_icon="👋",
+)
+
+st.write("# Welcome to CollegeGPT by JMFVitaeAI 👋")
+
+st.markdown(
+    """
+    Under construction
+    """
+)
